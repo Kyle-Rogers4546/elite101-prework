c075a8dca0395597e5b61f896cc4d71bd030c0dd
Kyle-Rogers4546 <kyle.rogers4546@gmail.com>
2025-11-13 23:01:20 +0000

    Commets

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 # 3 bank company help with seting up account
 
+# declare variables
 accountType = ""
 curAcountType = ""
 madeAccountTypes = []
 password = []
 accountNumber = []
+# intro print statments
 print("Welcome to the Chatbot!")
-name = input("What is your name: ")
+name = input("What is your name: ") # get name
 print("Hello " + name + "!")
-age = input("How old are you? ")
+age = input("How old are you? ") # get age
 print("")
 chating = True
 
 while chating:
+    # option print statments
     print("How may I help you " + name + ".")
     print("1. Create Account")
     print("2. Check Account")
     print("3. End")
-    answer = input("Please choose a option number: ")
+    answer = input("Please choose a option number: ") # get user inputs
     print("")
+    # end statments
     if answer == "3":
         print("Goodbye " + name + "!")
-        chating = False
+        chating = False # ends while loop
     elif answer == "2":
         print(f"Good chooice {name}!")
         while True:
-            answer = int(input("Please insert account number: "))
+            answer = int(input("Please insert account number: ")) # get user account number
             foundpassword = False
-            for i in range(len(accountNumber)):
+            for i in range(len(accountNumber)): # checks is account number right
                 if(answer == accountNumber[i]):
-                    curpassword = input(f"Okay now please insert your account password: ")
-                    if curpassword == password[i]:
+                    curpassword = input(f"Okay now please insert your account password: ") #get user password
+                    if curpassword == password[i]: # check if password right
                         print("Great here is your account information:")
                         print(f"Account Number: {accountNumber[i]}")
                         print(f"Password: {password[i]}")
@@ -47,18 +51,20 @@ while chating:
     elif answer == "1":
         print(f"Good choice {name}!")
         while True:
+            # option print statments
             print("Here are some account options.")
             print("1. Checking account")
             print("2. Saving account") 
             print("3. Cancle")
             accountType = input("Can you please choose a option number: ")
             print("")
+            # option 1
             if accountType == "1":
                 print("Ok a checking account.")
                 curAcountType = "checking"
                 print("")
-                password.append(input("Please create your account password: "))
-                accountNumber.append(1000 + len(accountNumber))
+                password.append(input("Please create your account password: ")) # save info
+                accountNumber.append(1000 + len(accountNumber)) # save info
                 print("Ok all good here is your information")
                 print(f"Account Number: {accountNumber[-1]}")
                 print(f"Password: {password[-1]}")
@@ -68,19 +74,20 @@ while chating:
                     print("Great!")
                     print(f"{name}, congrates on creating an account!")
                     print("Make sure to memorize your info!")
-                    madeAccountTypes.append(curAcountType)
+                    madeAccountTypes.append(curAcountType) # save info
                 else:
                     print("We hope you will try again!")
-                    password.pop
-                    accountNumber.pop
+                    password.pop # removes info
+                    accountNumber.pop # removes info
                 print("")
                 break
+            # option 2
             elif accountType == "2":
                 print("Ok a saving account.")
                 curAcountType = "saving"
                 print("")
-                password.append(input("Please create your account password: "))
-                accountNumber.append(1000 + len(accountNumber))
+                password.append(input("Please create your account password: ")) # saves info
+                accountNumber.append(1000 + len(accountNumber))  # saves info
                 print("Ok all good here is your information")
                 print(f"Account Number: {accountNumber[-1]}")
                 print(f"Password: {password[-1]}")
@@ -90,11 +97,11 @@ while chating:
                     print("Great!")
                     print(f"{name}, congrates on creating an account!")
                     print("Make sure to memorize your info!")
-                    madeAccountTypes.append(curAcountType)
+                    madeAccountTypes.append(curAcountType) # saves info
                 else:
                     print("We hope you will try again!")
-                    password.pop
-                    accountNumber.pop
+                    password.pop # removes info
+                    accountNumber.pop # removes info
                 print("")
                 break
             elif accountType == "3":
